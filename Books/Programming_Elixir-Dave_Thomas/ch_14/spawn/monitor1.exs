69a8455f25efb2c70c939fd26843dde810d1802a
Charles J. Schneider <schneider@br-tech.de>
2016-05-06 12:23:41 +0200

    ch_14 files; Programming Elixir

---
+++ Books/Programming_Elixir-Dave_Thomas/ch_14/spawn/monitor1.exs
@@ -0,0 +1,22 @@
+defmodule Monitor1 do
+  import :timer, only: [sleep: 1]
+
+  def sad_function do
+    sleep 500
+    exit(:boom)
+  end
+
+  def run do
+    res = spawn_monitor(Monitor1, :sad_function, [])
+    IO.puts inspect res
+    receive do
+      msg ->
+        IO.puts "Message Revieved #{inspect msg}"
+    after 1000 ->
+        IO.puts "Nothing Happened..."
+    end
+  end
+
+end
+
+Monitor1.run
